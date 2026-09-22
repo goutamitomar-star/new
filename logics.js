d26d2c7ba0c1590d6c27223e5408039ae8dbbdb0
goutamitomar-star <goutamitomar@gmail.com>
2026-09-22 16:03:07 +0530

    Add files via upload

---
+++ logics.js
@@ -0,0 +1,7 @@
+function login(){
+    document.querySelector('.log').classList.toggle('log-active')
+    document.querySelector('.log-card').classList.toggle('log-card-active')
+}
+function sign(){
+    window.location.href = 'account.html'
+}
